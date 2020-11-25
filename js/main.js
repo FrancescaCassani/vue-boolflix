b52ffdf33702df770c99d879b35402864191a9e1
Francesca <fracassani@gmail.com>
2020-11-25 17:10:07 +0100

    Inserimento ricerca film e serie tv

--- js/main.js
+++ js/main.js
@@ -2,19 +2,25 @@ const app = new Vue({
   el: '#app',
   data: {
     movies: [],
-    searchMovie: '',
-    languages : ['en', 'it'],
+    series: [],
+    searchBar: ''
   },
   created(){
 
   },
   methods: {
     getFilter() {
+      this.getFilterMovie();
+      this.getFilterSeries();
+
+      this.search = '';
+    },
+    getFilterMovie() {
       //Call API
       axios.get('http://api.themoviedb.org/3/search/movie', {
           params: {
             api_key: 'c59940771aef20931e51d0c89086e5a5',
-            query: this.searchMovie,
+            query: this.searchBar,
             language: 'it-IT'
          }
       })
@@ -23,15 +29,34 @@ const app = new Vue({
         console.log(response.data.results);
 
         //Ragionamento ricerca
-        if(this.searchMovie !== '') {
+        if(this.searchBar !== '') {
           this.movies = response.data.results;
         }
-
-        this.searchMovie = '';
-
       })
       .catch(error => {
         console.log('Movie not found', error);
+      })
+    },
+    getFilterSeries() {
+      //Call API
+      axios.get('http://api.themoviedb.org/3/search/tv', {
+          params: {
+            api_key: 'c59940771aef20931e51d0c89086e5a5',
+            query: this.searchBar,
+            language: 'it-IT'
+         }
+      })
+      .then( response => {
+        //Log di verifica
+        console.log(response.data.results);
+
+        //Ragionamento ricerca
+        if(this.searchBar !== '') {
+          this.series = response.data.results;
+        }
+      })
+      .catch(error => {
+        console.log('Serie not found', error);
       })
     },
     getStar(vote) {
@@ -39,6 +64,18 @@ const app = new Vue({
     }
   }
 });
+
+/* <span v-if="movie.original_language === 'en'">
+<img src="./img/en.png" alt="en">
+</span>
+
+<span v-else-if="movie.original_language === 'it'">
+<img src="./img/it.png" alt="it">
+</span>
+
+<span v-else>
+<p>{{movie.original_language}}</p>
+</span> */
 
 
 
